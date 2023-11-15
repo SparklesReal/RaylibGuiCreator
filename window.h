1eac028c1e1a4036717ad6d3da8b0620aabcf654
Sparkles <68858896+SparklesReal@users.noreply.github.com>
2023-11-15 23:17:48 +0100

    fixed spacing?

--- window.h
+++ window.h
@@ -3,15 +3,15 @@
 
 class WindowClass {
 private:
-	std::string title	= "GUI creator for Raylib";
-	int width			= 1920;
-	int height			= 1080;
+	std::string title = "GUI creator for Raylib";
+	int width			       = 1920;
+	int height			     = 1080;
 public:
 	std::string getTitle()	{ return title; }
-	int getWidth()			{ return width; }
+	int getWidth()			 { return width; }
 	int getHeight()			{ return height; }
 
-	void setWidth(int arg)	{ width = arg; }
+	void setWidth(int arg)	 { width = arg; }
 	void setHeight(int arg) { height = arg; }
 };
 
